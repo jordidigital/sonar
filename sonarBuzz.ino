201a825a398f28e87829bc09576154c94f2eae77
Jordi Roda <jordi.roda@eurecat.org>
2016-01-10 11:12:02 +0100

    more of code

---
+++ sonarBuzz.ino
@@ -0,0 +1,27 @@
+#define LOOP_DELAY  5000
+
+#define BUZZER 6
+#define BUZZER_SOUND 100
+#define MAX_DISTANCE 30
+
+#define SONAR_TRIG  2
+#define SONAR_ECHO  4
+
+int obstacleDistance;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(BUZZER, OUTPUT);
+  Serial.println("Sonar checking for obstacles.............");
+}
+
+void loop() {
+  obstacleDistance = getSonar();
+  if (obstacleDistance > MAX_DISTANCE || obstacleDistance <= 0){
+    Serial.println("Safe from any Obstacles......");
+    noTone(BUZZER);
+  } else {
+    tone(BUZZER, BUZZER_SOUND);
+  }  
+}
+
